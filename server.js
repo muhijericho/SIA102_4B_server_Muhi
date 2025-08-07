d15ed53d7b99d8e3a87fd06d4255d4f21e96e162
Jericho Muhi <138776450+muhijericho@users.noreply.github.com>
2025-08-07 19:41:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get(`/`, (req, res) => {
                 <title>My Express App</title>
             </head>
             <body >
-                <div style="text-align: center; font-family: Arial; width: 45%; height: 40%; background-color: #ddd9d9ff; margin-left: 500px;">
+                <div style="text-align: center; font-family: Arial; width: 45%; height: 40%; background-color: #ddd9d9ff; margin-left: 400px;">
                     <h1 style="color: red;">Server Under Maintenance</h1>
                     <p>We are currently performing scheduled maintenance. We apologize for any inconvenience this may cause.</p>
                     <p>Please check back later.</p>
@@ -23,5 +23,6 @@ app.get(`/`, (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
 
 
